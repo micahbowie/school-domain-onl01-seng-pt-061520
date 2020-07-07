da0c01d8f76681e9f6c46a4a93d36ee6704ddb44
Micah Bowie <micahbowie20@gmail.com>
2020-07-07 19:12:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,30 @@
 class School
-attr_reader :roster
+
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
-  
-  def add_student(my_name, grade)
-    roster[grade] = []
-    roster[grade] << my_name
-  end 
-  
-  def grade(my_grade)
-    return roster[my_grade]
-  end 
-    
-end 
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
