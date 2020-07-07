b7650ba2016e785283ec35bdf35ce9d1f229d01c
Micah Bowie <micahbowie20@gmail.com>
2020-07-07 17:23:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School 
+  attr_reader :name 
+  roster = {}
+  def initlialize(name)
+    @name = name 
+  end 
+  
+  def add_student(name, grade)
+    roster[grade] = []
+    roster[grade] << name 
+  end 
+    
+end 
